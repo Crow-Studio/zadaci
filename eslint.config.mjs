b0d553a2b3d57f816982410bc35256c5fb4ab537
thecodingmontana <dev.humblechris254@gmail.com>
2025-06-26 11:03:56 +0300

    chore(app): update eslint rules and add commintlint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,13 @@
-// @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
-  // Your custom configs here
+  {
+    name: 'thecodingmontana/tailwind',
+    rules: {
+      'tailwindcss/no-custom-classname': 'off',
+      'vue/no-v-html': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'vue/require-default-prop': 'off',
+    },
+  },
 )
