7087cea09f0cbbdea9b0791ef5c160f594c73725
thecodingmontana <dev.humblechris254@gmail.com>
2025-09-06 18:46:54 +0300

    chore(app): added drizzle query logger

--- server/utils/drizzle.ts
+++ server/utils/drizzle.ts
@@ -1,7 +1,8 @@
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
+import { EnhancedQueryLogger } from 'drizzle-query-logger'
+import pino from 'pino'
 import * as schema from '../database/schema'
-
 import 'dotenv/config'
 
 export { and, eq, or, sql } from 'drizzle-orm'
@@ -10,8 +11,16 @@ export const tables = schema
 
 const connectionString = process.env.DATABASE_URL!
 
+const pinoLogger = pino()
+
+const logger = new EnhancedQueryLogger({
+  log: (message) => {
+    pinoLogger.info(message)
+  },
+})
+
 const queryClient = postgres(connectionString, { prepare: false })
 
 export function useDrizzle() {
-  return drizzle(queryClient, { schema })
+  return drizzle(queryClient, { schema, logger })
 }
